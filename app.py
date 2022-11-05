6d695e8a07ef8e9cbbecfdbe52c8066366468d8f
M3galodn81 <dalisdism0@gmail.com>
2022-11-05 16:44:36 +0800

    Updated App

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', '0') #0 being off 1 being on as in true/false
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '640')
 
 class WelcomeScreen(Screen):
     pass
