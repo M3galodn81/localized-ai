eb3a9bc997fce7c56eeaf76bf7615aebb4610fd2
M3galodn81 <dalisdism0@gmail.com>
2022-11-12 23:02:30 +0800

    Added a simple VA

--- ai/main.py
+++ ai/main.py
@@ -1,2 +1,51 @@
+from gtts import gTTS
+from playsound import playsound
+import random, os, datetime, string
+import speech_recognition as sr
 
-print("Test")
+def get_random_string(length:int):
+    letters = string.ascii_lowercase
+    result_string = ''.join(random.choice(letters) for _ in range(length))
+    return result_string
+
+def bigkas(args):
+    tts = gTTS(text=args,lang='tl')
+    random_string = get_random_string(4)
+    audio_file = os.path.dirname(random_string)+random_string+'-audio.mp3'
+    tts.save(audio_file)
+    playsound(audio_file)
+    print(args)
+    os.remove(audio_file)
+
+def oras():
+    hour = datetime.datetime.now()
+    time = datetime.datetime.now().strftime('%I %M')
+    if hour >=0 and hour < 12:
+        meridiem = 'AM'
+    meridiem = 'PM'
+
+    bigkas(f'Ang oras ngayon ay {time} {meridiem}')
+
+while True:
+    with sr.Microphone() as source:
+        print('nakikinig')
+        audio = sr.Recognizer().listen(source)
+        try:
+            queue = sr.Recognizer().recognize_google(audio, language='tl')
+            salita = str(queue).lower
+
+            if 'kamusta' in salita and 'ka' in salita:
+                bigkas('okay lang po')
+                print('kamusta')
+                continue 
+            
+            if 'anong' in salita and 'oras' in salita:
+                oras()
+                print(f'oras')
+                continue
+            
+            print(f'{salita}')
+
+        except Exception as e:
+            print(e)
+            continue
