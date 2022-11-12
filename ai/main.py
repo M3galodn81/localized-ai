03f438c8a72aabcae0b22733ab5ea84846cdcb1f
M3galodn81 <dalisdism0@gmail.com>
2022-11-12 23:06:03 +0800

    Update main.py to add comments

--- ai/main.py
+++ ai/main.py
@@ -8,7 +8,7 @@ def get_random_string(length:int):
     result_string = ''.join(random.choice(letters) for _ in range(length))
     return result_string
 
-def bigkas(args):
+def bigkas(args): # TTS (Tagalog)
     tts = gTTS(text=args,lang='tl')
     random_string = get_random_string(4)
     audio_file = os.path.dirname(random_string)+random_string+'-audio.mp3'
@@ -18,33 +18,33 @@ def bigkas(args):
     os.remove(audio_file)
 
 def oras():
-    hour = datetime.datetime.now()
+    hour = datetime.datetime.now() 
     time = datetime.datetime.now().strftime('%I %M')
-    if hour >=0 and hour < 12:
+    if hour >=0 and hour < 12: #if AM or PM
         meridiem = 'AM'
     meridiem = 'PM'
 
     bigkas(f'Ang oras ngayon ay {time} {meridiem}')
 
 while True:
-    with sr.Microphone() as source:
+    with sr.Microphone() as source: #if mircophone detects sound
         print('nakikinig')
         audio = sr.Recognizer().listen(source)
         try:
             queue = sr.Recognizer().recognize_google(audio, language='tl')
             salita = str(queue).lower
 
-            if 'kamusta' in salita and 'ka' in salita:
+            if 'kamusta' in salita and 'ka' in salita: # kamusta ka function
                 bigkas('okay lang po')
                 print('kamusta')
                 continue 
             
-            if 'anong' in salita and 'oras' in salita:
+            if 'anong' in salita and 'oras' in salita: #anong oras function
                 oras()
                 print(f'oras')
                 continue
             
-            print(f'{salita}')
+        #     print(f'{salita}')
 
         except Exception as e:
             print(e)
