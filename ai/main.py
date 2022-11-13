5d312b19def1b20ff8a4216a39779bedd3c04208
M3galodn81 <dalisdism0@gmail.com>
2022-11-13 14:40:09 +0800

    Updated main.py/bigkas()

--- ai/main.py
+++ ai/main.py
@@ -2,6 +2,8 @@ from gtts import gTTS
 from playsound import playsound
 import random, os, datetime, string
 import speech_recognition as sr
+from io import BytesIO
+from tempfile import NamedTemporaryFile
 
 def get_random_string(length:int):
     letters = string.ascii_lowercase
@@ -9,13 +11,21 @@ def get_random_string(length:int):
     return result_string
 
 def bigkas(args): # TTS (Tagalog)
-    tts = gTTS(text=args,lang='tl')
-    random_string = get_random_string(4)
-    audio_file = os.path.dirname(random_string)+random_string+'-audio.mp3'
-    tts.save(audio_file)
-    playsound(audio_file)
-    print(args)
-    os.remove(audio_file)
+    # tts = gTTS(text=args,lang='tl')
+    # random_string = get_random_string(4)
+    # audio_file = os.path.dirname(random_string)+random_string+'-audio.mp3'
+    # tts.save(audio_file)
+    # playsound(audio_file)
+    # print(args)
+    # os.remove(audio_file)
+
+    # mp3_fp = BytesIO()
+    # tts = gTTS(text = args, lang='tl')
+    # tts.write_to_fp(mp3_fp)
+
+    gTTS(text=args, lang='tl').write_to_fp(voice := NamedTemporaryFile())
+    playsound(voice.name)
+    voice.close()
 
 def oras():
     hour = datetime.datetime.now() 
@@ -30,10 +40,12 @@ while True:
     with sr.Microphone() as source: #if mircophone detects sound
         print('nakikinig')
         audio = sr.Recognizer().listen(source)
+        
         try:
             queue = sr.Recognizer().recognize_google(audio, language='tl')
-            salita = str(queue).lower
-
+            # salita = str(queue).lower
+            salita = str('kamusta ka, anong oras')
+        
             if 'kamusta' in salita and 'ka' in salita: # kamusta ka function
                 bigkas('okay lang po')
                 print('kamusta')
