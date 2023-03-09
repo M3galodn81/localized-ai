0a094dc49dd633b57d1b81df60b1bcee263bbde1
M3galodn81 <dalisdismark0@gmail.com>
2023-03-09 14:15:35 +0800

    A

--- ai/voice_input.py
+++ ai/voice_input.py
@@ -5,7 +5,7 @@ import text_analzyer
 def speak(audio):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    engine.setProperty('voices', 'filipino')
+    engine.setProperty('voice', voices[0].id)
     engine.say(audio)
     engine.runAndWait()
 
